3b3ce11fede081a0964c4bc7d0b553eb7f86ec3b
hyperhoo <utrans77@naver.com>
2022-05-24 20:06:34 +0900

    make it better

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,24 +1,14 @@
 import express from 'express';
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static('build'));
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + '/build/index.html');
 });
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
   console.log('server is running!');
 });
-
-// client build -> build 폴더 -> server/build -> 서버를 실행
-
-// client 폴더로 이동
-// npm ci
-// npm run build
-// client /build -> server/build 폴더로 이동
-
-// server 폴더로 이동
-// npm ci
-// tsc
-// node app.js
